2e32935e24eef2bbfe60592ea026bc46a4686d84
CUXUM <mcbrayan20@gmail.com>
2024-05-05 21:22:36 -0600

    se ahrego un h3

--- prueba2.php
+++ prueba2.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
+<h1>esta es una prueba</h1>
+<h3>esta es una prueba 3</h3>
+<h4>dflkjfsdjljdsjkjjsdlfjlsljkfj</h4>
+
+
+
+
+
+
+    
+</body>
+</html>
